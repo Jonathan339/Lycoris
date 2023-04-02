9a64efa03e993a4eb8ebd852d8823091ec2f8007
Jonathan D. Dominguez <30633652+Jonathan339@users.noreply.github.com>
2023-04-02 12:23:34 -0300

    se comento el código

--- driver/server.py
+++ driver/server.py
@@ -1,15 +1,16 @@
 import json
 import subprocess
 
-
 class Server:
-
     def __init__(self):
         self.process = ""
 
     def start_server(self, driver, port=5050):
+        # Se verifica que el driver proporcionado sea válido
         if str.lower(driver) == "chrome" or str.lower(driver) == "gecko":
+            # Se inicia el servidor utilizando el driver y el puerto especificado
             self.process = subprocess.Popen(driver + "--port=" + port)
 
     def kill_server(self):
+        # Se detiene el proceso del servidor
         self.process.terminate()
